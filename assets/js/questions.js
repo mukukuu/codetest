9ef1d7e731c11d33ec1479af6f19c81ced600ce1
mukukuu <qinii@me.com>
2021-11-30 08:58:46 +0100

    created questions.js and linked to html

---
+++ assets/js/questions.js
@@ -0,0 +1,46 @@
+/* QUIZ QUESTIONS ARRAY*/
+var Qs = [ 
+    /* question 1 */
+    {
+        Q: "Which element is used for closing a line of code?",
+        options: [":", ".", ";", "}"],
+        answer: ";"
+    },
+
+    /* question 2 */
+    {
+        Q:"Which line of code can correctly links a CSS stylesheet?",
+        options:['<link rel="stylesheet" href="adress" />', '<links ref="styleshet" href="adress" />', '<link rel="stylesheet" href="adress" /', '<link css="stylesheet" href="adress" />'],
+        answer: '<link rel="stylesheet" href="adress" />'
+    },
+
+    /* question 3 */
+    {
+        Q: "How to comment out a block of js codes on MAC?",
+        options: ["/", "--command +++-", "command + /", "/!--/"],
+        answer: "command + /"
+    },
+
+    /* question 4 */
+    {
+        Q: "Is it illegal to use code library?",
+        options: ["illigal", "legal", "illegal", "I don't know"],
+        answer: "legal"
+    },
+
+    /* question 5 */
+    {
+        Q: "what is the most common reason that your code doesn't work?",
+        options: ["messy typing", "typo", "no internet connection", "unsaved works"],
+        answer: "typo"
+    },
+     
+    /* question 6 */
+       {
+        Q: "what is the meaning of 'Var' ?",
+        options: ["var statement", "variable function", "storage", "var input"],
+        answer: "var statement"
+       }
+        
+
+];
